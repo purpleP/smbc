d0a470cdd055d3c75f2e66d6852f5c3b5199eaac
Michael Doronin <warrior2031@mail.ru>
2020-11-05 17:16:57 +0300

    wip

--- build.rs
+++ build.rs
@@ -4,25 +4,23 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    match pkg_config::probe_library("smbclient") {
-        Ok(_) => {
-            if cfg!(target_os = "macos") {
-                println!("cargo:rustc-flags=-L /usr/local/lib -l smbclient");
-            } else {
-                println!("cargo:rustc-flags=-l smbclient");
-            }
-        }
-        Err(e) => {
-            println!("Error: SMB Client library not found!");
-            panic!("{}", e);
-        }
-    };
+    let smbclient_lib = pkg_config::probe_library("smbclient").unwrap();
     println!("cargo:rustc-link-lib=smbclient");
     println!("cargo:rerun-if-changed=wrapper.h");
+    let include_args = smbclient_lib
+        .include_paths
+        .iter()
+        .flat_map(|path| path.to_str())
+        .map(|raw_path| format!("-I{}", raw_path));
+    let libs_args = smbclient_lib
+        .libs
+        .iter()
+        .map(|l| format!("-l{}", l));
     let bindings = bindgen::Builder::default()
         .generate_comments(false)
         .header("wrapper.h")
-        .clang_arg("-I/usr/include/samba-4.0")
+        .clang_args(include_args)
+        .clang_args(libs_args)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
